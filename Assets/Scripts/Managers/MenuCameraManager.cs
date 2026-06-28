e01af52ed01bd70c9fadeb621ae63f7644a03440
Evy Avila <384036@colum.edu>
2026-06-28 16:28:46 -0500

    Making progress on menus and transitions

--- Assets/Scripts/Managers/MenuCameraManager.cs
+++ Assets/Scripts/Managers/MenuCameraManager.cs
@@ -2,16 +2,22 @@ using UnityEngine;
 using System.Linq;
 using System.Collections.Generic;
 using System;
+using Unity.Cinemachine;
 
-public enum menuType { MainMenu, QuitMenu, LeaderboardMenu, SettingsMenu, GameplayMenu}
+public enum menuType { MainMenu, QuitMenu, LeaderboardMenu, SettingsMenu, GameplayMenu, RestartMenu}
 
 public class MenuCameraManager : MonoBehaviour
 {
     public List<VirtualCameraMenu> menuVirtualCameras;
 
+    
+    int playerCamPosition;
+
     void Start()
     {
         UpdateCurrentCamera(menuType.MainMenu);
+
+        playerCamPosition = menuVirtualCameras.FindIndex(x => x.currentMenu == menuType.GameplayMenu);
     }
 
     private void OnEnable()
@@ -27,20 +33,19 @@ public class MenuCameraManager : MonoBehaviour
 
     private void UpdateCurrentCamera(menuType menu)
     {
+        
+
         foreach (var menuItem in menuVirtualCameras) 
         {
             if (menuItem.currentMenu == menu) 
             {
                 menuItem.menuCamera.SetActive(true);
-                //return;
             }
             else
             {
                 menuItem.menuCamera.SetActive(false);
 
-            }
-
-                
+            }     
         }
     }
     
--- Assets/Scripts/Menus/FadeTransition.cs
+++ Assets/Scripts/Menus/FadeTransition.cs
@@ -1,37 +1,88 @@
 using NUnit.Framework;
+using System.Collections;
 using System.Collections.Generic;
 using Unity.Cinemachine;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class FadeTransition : MonoBehaviour
 {
-    public List<CinemachineVirtualCameraBase> vCams;
+    [SerializeField]
+    private CinemachineVirtualCameraBase alignCam, zoomCam, playerCam, restartCam;
 
     [SerializeField]
-    private float currentTimer, maxTimer;
+    private float maxTimer, maxFadeTimer;
 
-    bool isCompleted;
+    private float currentTimer;
+
+    [SerializeField]
+    private Image fadeImage;
+
+    private float fadeAmout;
+
+    private bool isPlayerCameraActive;
 
     void Awake()
     {
+        maxFadeTimer = maxFadeTimer == 0 ? 0.5f : maxFadeTimer;
         currentTimer = maxTimer;
-
-        foreach(var cam in vCams)
-        {
-            cam.gameObject.SetActive(false);
-        }
+        SetActiveCameras(false, false);
+        
+        playerCam.gameObject.SetActive(false);
+        fadeImage.color = new Color(fadeImage.color.r, fadeImage.color.g, fadeImage.color.b, 0);
     }
 
     private void Update()
     {
-        if (!vCams[0].IsParticipatingInBlend() && vCams[0].gameObject.activeInHierarchy && !isCompleted)
+        if (!alignCam.IsParticipatingInBlend() && alignCam.gameObject.activeInHierarchy && alignCam.IsLive)
         {
-            WaitTime();
+            WaitTime(true, false, 0);
         }
+
+        if (!zoomCam.IsParticipatingInBlend() && zoomCam.gameObject.activeInHierarchy && zoomCam.IsLive)
+        {
+            WaitTime(false, false, 1);
+        }
+
+        SwitchToPlayerCamera();
+
+    }
+
+
+    private void OnEnable()
+    {
         
     }
 
-    private void WaitTime()
+    private void OnDisable()
+    {
+        
+    }
+
+    //
+    private void SwitchToPlayerCamera()
+    {
+        if (fadeImage.color.a > 0 && !zoomCam.IsLive)
+        {
+            if (!playerCam.IsLive && !isPlayerCameraActive)
+            {
+                playerCam.gameObject.SetActive(true);
+                isPlayerCameraActive = true;
+            }
+
+            fadeAmout -= Time.deltaTime * maxFadeTimer;
+            fadeImage.color = new Color(fadeImage.color.r, fadeImage.color.g, fadeImage.color.b, fadeAmout);
+        }
+    }
+
+    private void SetPlayerActive()
+    {
+        playerCam.gameObject.SetActive(false);
+        isPlayerCameraActive = false;
+    }
+
+    //Waiting to transition from the aim camera to the zoom camera
+    private void WaitTime(bool activateZoom, bool activateAlign, int num)
     {
         if (currentTimer > 0) 
         {
@@ -39,19 +90,16 @@ public class FadeTransition : MonoBehaviour
         }
         else
         {
-            vCams[1].gameObject.SetActive(true);
             currentTimer = maxTimer;
-            isCompleted = true;
-            /*foreach (var cam in vCams)
-            {
-                cam.gameObject.SetActive(false);
-            }*/
+            SetActiveCameras(activateZoom, activateAlign);
+            fadeImage.color = new Color(fadeImage.color.r, fadeImage.color.g, fadeImage.color.b, num);
+            fadeAmout = num;
         }
-
-            
     }
-    
 
-
-
+    private void SetActiveCameras(bool isZoomActive, bool isAlignActive)
+    {
+        zoomCam.gameObject.SetActive(isZoomActive);
+        alignCam.gameObject.SetActive(isAlignActive);
+    }
 }
